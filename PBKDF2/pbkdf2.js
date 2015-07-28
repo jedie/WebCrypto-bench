a9f707ff119d40dda1484305a9f5300e1e3ae726
JensDiemer <git@jensdiemer.de>
2015-07-28 11:58:49 +0200

    add first working PBKDF2 test

---
+++ PBKDF2/pbkdf2.js
@@ -0,0 +1,94 @@
+/**********************************************************************
+    Written by Jens Diemer
+    :license: GNU GPL v3 or above, see LICENSE for more details
+    :homepage: https://github.com/jedie/WebCrypto-bench
+/*********************************************************************/
+
+try {
+    jQuery(document);
+} catch (e) {
+    alert("Error, jQuery JS not loaded!\n Original error was:" + e);
+}
+
+function pbkdf2(txt, salt, iterations, bytes) {
+    if (typeof(bytes)==='undefined') bytes = PBKDF2_BYTE_LENGTH;
+
+    log("pbkdf2 calc with iterations: " + iterations + " - bytes: " + bytes)
+
+    // TODO: add work-a-round if TextEncoder not supported
+    // IE / Safari, see:
+    // https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
+    txt = new TextEncoder("utf-8").encode(txt);
+
+    return window.crypto.subtle.importKey(
+        "raw", txt, {name: "PBKDF2"},
+        false, //whether the key is extractable (i.e. can be used in exportKey)
+        ["deriveBits"] // ["deriveKey", "deriveBits"] //can be any combination of "deriveKey" and "deriveBits"
+    ).then(function(key){
+        salt = new TextEncoder("utf-8").encode(salt);
+        return window.crypto.subtle.deriveBits(
+            {
+                "name": "PBKDF2",
+                salt: salt,
+                iterations: iterations,
+                hash: {name: "SHA-1"},
+            },
+            key, bytes*8
+        ).then(function(hash){ // get a ArrayBuffer back
+            var hex_hash=hex(hash);
+            log("The derived " + (bytes*8) + "-bit key is: " + hex_hash);
+            return hex_hash;
+        })
+    })
+}
+
+
+function test_pbkdf2_js() {
+    window.stdout("Test PBKDF2...");
+
+    log("TEST_STRING='"+TEST_STRING+"'")
+    return pbkdf2(
+        txt=TEST_STRING,
+        salt=TEST_STRING,
+        iterations=5,
+        bytes=16
+    ).then(function(hex_hash){
+        var should_be = '4460365dc7df037dbdd851f1ffed7130';
+        if (hex_hash == should_be) {
+            window.stdout("Check PBKDF2 passed.\n");
+        } else {
+            low_level_error("PBKDF2 test failed!\n'" + hex_hash + "' != '" + should_be + "'");
+        }
+    }).catch(function(err){
+        low_level_error("Error running PBKDF2 test:" + err);
+    });
+}
+
+$(document).ready(function() {
+    init_out();
+    window.stdout("Check WebCrypto API...");
+    check_webcrypto();
+
+    test_pbkdf2_js();
+
+    $("form").submit(function() {
+        window.stdout("calculate PBKDF2...");
+
+        var start_time = new Date();
+        var iterations = parseInt($("#iterations").val())
+        pbkdf2(
+            txt=$("#test_string").val(),
+            salt=$("#test_salt").val(),
+            iterations=iterations,
+            bytes=parseInt($("#bytes").val())
+        ).then(function(hex_hash){
+            var duration = new Date() - start_time;
+            window.stdout("Generated PBKDF2 hash:"+hex_hash+"\n");
+            window.stdout("duration: " + duration/1000+"sec for "+iterations+" iterations.\n");
+        }).catch(function(err){
+            low_level_error("Error running PBKDF2 test:" + err);
+        });
+
+        return false;
+    });
+});
---
+++ base_files/base.js
@@ -0,0 +1,71 @@
+/**********************************************************************
+    Written by Jens Diemer
+    :license: GNU GPL v3 or above, see LICENSE for more details
+    :homepage: https://github.com/jedie/WebCrypto-bench
+/*********************************************************************/
+
+var TEST_STRING = " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// helper function for console logging
+// set debug to true to enable debug logging
+function log() {
+    if (window.console && window.console.log) {
+        try {
+            window.console.log(Array.prototype.join.call(arguments,''));
+        } catch (e) {
+            log("Error:" + e);
+        }
+
+    }
+}
+log("JS logging initialized");
+
+function low_level_error(msg) {
+    log("Low level error: "+msg);
+    window.stdout("\nERROR:\n"+msg+"\n");
+    //alert(msg);
+    throw "ERROR: " + msg;
+}
+
+function hex(buffer) {
+    // from example here:
+    // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+    var hexCodes = [];
+    var view = new DataView(buffer);
+    for (var i = 0; i < view.byteLength; i += 4) {
+        // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+        var value = view.getUint32(i)
+        // toString(16) will give the hex representation of the number without padding
+        var stringValue = value.toString(16)
+        // We use concatenation and slice for padding
+        var padding = '00000000'
+        var paddedValue = (padding + stringValue).slice(-padding.length)
+        hexCodes.push(paddedValue);
+    }
+    return hexCodes.join(""); // Join all the hex strings into one
+}
+
+function init_out() {
+    out = $("#output");
+    window.stdout = function(data) {
+        out.append(data);
+    }
+    window.stdout.reset = function(data) {
+        out.empty();
+    }
+}
+
+function check_webcrypto() {
+    // IE11 has window.msCrypto but doesn't provide promise .then() / .catch()
+    // it used the outdated .oncomplete() and .onerror()
+    if (!window.crypto) {
+        low_level_error("Your browser doesn't support Web Cryptography API: 'window.crypto' doesn't exists!");
+    }
+    if (window.crypto.webkitSubtle) {  // Safari
+        window.crypto.subtle = window.crypto.webkitSubtle;
+    }
+    window.stdout("Web Cryptography API is generally supported: 'window.crypto' exists.\n");
+    if (!window.crypto.subtle) {
+        low_level_error("Your browser doesn't support Web Cryptography API: 'window.crypto.subtle' doesn't exists!");
+    }
+}
